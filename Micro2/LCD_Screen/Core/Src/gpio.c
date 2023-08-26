ee74aa23aac00913be97aeb4db23a81d86751d0e
Emanuel600 <78040463+Emanuel600@users.noreply.github.com>
2023-08-26 12:37:31 -0300

    modified:   Micro2/LCD_Screen/Core/Src/gpio.c

--- Micro2/LCD_Screen/Core/Src/gpio.c
+++ Micro2/LCD_Screen/Core/Src/gpio.c
@@ -58,7 +58,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pins : PB0 PB1 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA8 PA9 */
